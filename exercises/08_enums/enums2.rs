b1547853cc7882ab3125eb1486d482c733a350c1
Jose <cordjr@gmail.com>
2025-01-31 09:55:30 +0000

    initial commit

---
+++ exercises/08_enums/enums2.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+struct Size {
+    width: u32,
+    height: u32,
+}
+
+#[derive(Debug)]
+enum Message {
+    // TODO: Define the different variants used below.
+
+    Move(Point),
+    ChangeColor(u16,u16,u16),
+    Resize { width: i32, height: i32 },
+    Echo(String),
+    Quit
+}
+
+
+
+impl Message {
+    fn call(&self) {
+        println!("{self:?}");
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Resize {
+            width: 10,
+            height: 30,
+        },
+        Message::Move(Point { x: 10, y: 15 }),
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
